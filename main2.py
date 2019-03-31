56affa8f2585738ab843d3d89a12a1d391342515
yonkers <toplenboren@yandex.ru>
2019-03-31 23:21:13 +0500

    added the functions of saving / loading the text file

--- main2.py
+++ main2.py
@@ -3,27 +3,43 @@ import sys
 
 buffer = ""
 
-def exit_on_q(key):
-    if key in ('q', 'Q'):
+def exit_on_enter(key):
+    if key == 'enter':
+        save_text('try.txt', edit.edit_text)
         raise urwid.ExitMainLoop()
 
 class TextField(urwid.Filler):
+
     def keypress(self, size, key):
         if key != 'enter':
             return super(TextField, self).keypress(size,key)
+        edit.edit_text+='\n'
         self.original_widget = urwid.Text(
             u"Saved some data: \n %s \n symbols" % len(edit.edit_text)
         )
         buffer = edit.edit_text
 
 
-palette = [('I say', 'default,bold', 'default', 'bold'),('bg', 'black', 'dark blue')]
+def open_text(url):
+    raw = open(url, 'r')
+    msg = raw.read()
+    raw.close()
+    return msg
+
+
+def save_text(url, text):
+    raw = open(url, 'w')
+    raw.write(text)
+    raw.close()
+
+
+palette = [('bg', 'black', 'dark blue'),('banner', 'bold', 'white'  )]
 
 edit = urwid.Edit()
-edit.edit_text = "Try to edit me"
+edit.edit_text = open_text('try.txt')
 editboy = TextField(edit)
 
-text_helper = urwid.Text(u"A simple text editor")
+text_helper = urwid.Text(('banner', u"A simple text editor"), align='center')
 letter_counter = urwid.Text(u'Symbols count:')
 div = urwid.Divider()
 pile = urwid.Pile([text_helper, div, edit, div, letter_counter])
@@ -35,5 +51,5 @@ def on_editboy_change(edit, new_edit_text):
 
 urwid.connect_signal(edit,'change', on_editboy_change)
 
-loop = urwid.MainLoop(top, palette, unhandled_input=exit_on_q)
+loop = urwid.MainLoop(top, palette, unhandled_input=exit_on_enter)
 loop.run()
