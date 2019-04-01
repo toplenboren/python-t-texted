02bc908645f7d04d6287f7c3351d89ff585b3bf9
yonkers <toplenboren@yandex.ru>
2019-04-02 00:18:59 +0500

    Improved Sys.argv integration, Added -h / --help

--- Sandbox.py
+++ Sandbox.py
@@ -1,13 +1,4 @@
 ### A FILE THAT WAS CREATED ONLY FOR EDUCATIONAL PURPOSES, DON'T USE IN PRODUCTION ###
+import sys
 
-import urwid
-
-def show_or_exit(key):
-    if key in ('q', 'Q'):
-        raise urwid.ExitMainLoop()
-    txt.set_text(repr(key))
-
-txt = urwid.Text(u"Hello World")
-fill = urwid.Filler(txt, 'top')
-loop = urwid.MainLoop(fill, unhandled_input=show_or_exit)
-loop.run()
+print(sys.argv)
--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import sys
 import urwid
 from settings import PALETTE
 
-
 def exit_on_enter(key):
     if key == 'enter':
-        save_text('try.txt', edit.edit_text)
+        save_text(url, edit.edit_text)
         raise urwid.ExitMainLoop()
 
 
@@ -23,30 +22,58 @@ def on_edit_change(edit, new_edit_text):
 
 
 def open_text(url):
-    raw = open(url, 'r')
+    try:
+        raw = open(url, 'r')
+    except FileNotFoundError:
+        print("ERROR, File was not found")
+        exit(-1)
     msg = raw.read()
     raw.close()
     return msg
 
 
 def save_text(url, text):
-    raw = open(url, 'w')
+    try:
+        raw = open(url, 'w')
+    except FileNotFoundError:
+        print("ERROR, File was not found")
+        exit(-1)
     raw.write(text)
     raw.close()
 
 
-url = sys.argv[1]
+def print_usage():
+    print("Usage: python3 main.py [path to the text file]")
+    print("-h or --help for additional info")
 
-edit = urwid.Edit()
-edit.edit_text = open_text(url)
 
-text_helper = urwid.Text(('banner', u"A simple text editor"), align='center')
-letter_counter = urwid.Text(u'Symbols count:')
-div = urwid.Divider()
-pile = urwid.Pile([text_helper, div, edit, div, letter_counter])
-edit_window = urwid.Filler(pile, valign='top')
+def print_light_help():
+    print("light help")
 
-urwid.connect_signal(edit, 'change', on_edit_change)
 
-loop = urwid.MainLoop(edit_window , PALETTE , unhandled_input=exit_on_enter)
-loop.run()
+def print_full_help():
+    print("full_help")
+
+
+if len(sys.argv) == 1:
+    print_usage()
+elif sys.argv[1] == '-h':
+    print_light_help()
+elif sys.argv[1] == '--help':
+    print_full_help()
+else:
+    url = sys.argv[1]
+
+    edit = urwid.Edit()
+    edit.edit_text = open_text(url)
+
+    text_helper = urwid.Text(('banner', u"A simple text editor"), align='center')
+    letter_counter = urwid.Text(u'Symbols count:')
+    div = urwid.Divider()
+    pile = urwid.Pile([text_helper, div, edit, div, letter_counter])
+    edit_window = urwid.Filler(pile, valign='top')
+
+    urwid.connect_signal(edit, 'change', on_edit_change)
+
+    loop = urwid.MainLoop(edit_window , PALETTE , unhandled_input=exit_on_enter)
+    loop.run()
